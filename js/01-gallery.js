834773d89b0e6c139805ad4882bca53a034e6ef8
WMD11 <113972186+WMD11@users.noreply.github.com>
2023-11-15 22:57:47 +0200

    HW7, commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
 console.log(galleryItems);
+
+const galleryRef = document.querySelector(".gallery"); 
+const CreateGalleryMarkup = (items) => {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+     <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+       />
+     </a>
+    </li>
+ `
+    )
+    .join("");
+};
+
+galleryRef.innerHTML = CreateGalleryMarkup(galleryItems); 
+const openModal = (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+
+  const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      instance.close();
+    }
+  });
+};
+
+galleryRef.addEventListener("click", openModal);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+
+galleryItems.forEach((item) => {
+  const galleryListItem = document.createElement("li"); 
+  galleryListItem.classList.add("gallery__item"); 
+  
+  galleryListItem.innerHTML = `
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+    />
+  </a>`;
+  galleryContainer.append(galleryListItem); 
+});
+
+galleryContainer.addEventListener("click", preventDefaultClick);
+
+function preventDefaultClick(e) {
+  e.preventDefault();
+}
+
+
+const galleryLightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt", 
+  captionDelay: 250, 
+});
+
+console.log(galleryLightbox);
